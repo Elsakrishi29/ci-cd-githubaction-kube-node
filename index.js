b8cfea806f52b21f1857918684dc50a2668263c9
Elsakrishi29 <krishveni2902@gmail.com>
2025-11-10 05:17:46 -0500

    change index file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({ message: 'Hello from myapp', ts: Date.now() }));
+  res.end(JSON.stringify({ message: 'Hello from my ci-cd githubaction kube node', ts: Date.now() }));
 });
 
 server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
